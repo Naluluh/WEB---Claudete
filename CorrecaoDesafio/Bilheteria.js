9002760e196a583f042409a5bc77fc7d7b82e497
Naluluh <nalu16luna@gmail.com>
2026-05-10 22:26:39 -0300

    Add logic to handle missing and error boxes

--- CorrecaoDesafio/Bilheteria.js
+++ CorrecaoDesafio/Bilheteria.js
@@ -1,1 +1,17 @@
+// Conteúdo de cada caixa (definição de variável: array)
+let caixas = ['Papel', 'Clips', 'Grampo', 'Elastico', 'Caneta', 'Lapis', 'Borracha', 'Tesoura', 'ERRO', 'Tinta'];
 
+// Laço de repetição: contagem das caixas
+for (let i = 0; i < caixas.length; i++) {
+
+    // Desvio condicional: caixas extraviadas
+    if (i == 4 || i == 7) { // Retirada do '$'
+        continue; // Pula iteração
+    }
+
+    // Desvio condicional: caixa com erro
+    if (caixas[i] == "ERRO") 
+        break; // Encerra o laço
+
+    console.log("Caixa conferida: " + (i + 1) + ": " + caixas[i]); // Exibe no console a caixa e seu conteúdo
+}
