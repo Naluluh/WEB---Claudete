bb38838c7be1e631ed82e96342623cf90a613923
Naluluh <nalu16luna@gmail.com>
2026-05-11 17:11:02 -0300

    Refactor almoxarifadoCorrigido function
    Refactor the almoxarifadoCorrigido function to improve readability and structure.

--- CorrecaoDesafio/Bilheteria.js
+++ CorrecaoDesafio/Bilheteria.js
@@ -1,17 +1,35 @@
-// Conteúdo de cada caixa (definição de variável: array)
-let caixas = ['Papel', 'Clips', 'Grampo', 'Elastico', 'Caneta', 'Lapis', 'Borracha', 'Tesoura', 'ERRO', 'Tinta'];
+// função
+function almoxarifadoCorrigido(){
+    // Conteúdo de cada caixa (definição de variável: array)
+    let itens = document.getElementById("listaCaixas");
+    let mensagem = document.getElementById("mensagem");
+    let caixas = ['Papel', 'Clips', 'Grampo', 'Elastico', 'Caneta', 'Lapis', 'Borracha', 'Tesoura', 'ERRO', 'Tinta'];
+    let relatorio = document.getElementById("relatorio");
+    let lista = document.createElement("li");
+    
+    // Laço de repetição: contagem das caixas
+    for (let i = 0; i < caixas.length; i++) {
 
-// Laço de repetição: contagem das caixas
-for (let i = 0; i < caixas.length; i++) {
+        // Desvio condicional: caixas extraviadas
+        if (i == 4 || i == 7) { // Retirada do '$'
+            continue; // Pula iteração
+        }
 
-    // Desvio condicional: caixas extraviadas
-    if (i == 4 || i == 7) { // Retirada do '$'
-        continue; // Pula iteração
+        // Desvio condicional: caixa com erro
+        if (caixas[i] == "ERRO") {
+
+            // Mensagem com erro
+            lista.innerHTML += "<p style='color:red'>ERRO na caixa " + (i + 1) + "</p>";
+            break;
+        }
+
+        // Mensagem exibida
+        lista.innerHTML += "Caixa conferida: " + (i + 1) + ": " + caixas[i] + "<br>"; 
     }
 
-    // Desvio condicional: caixa com erro
-    if (caixas[i] == "ERRO") 
-        break; // Encerra o laço
+    // Espaço entre listas
+    lista.style.marginBottom = "30px";
 
-    console.log("Caixa conferida: " + (i + 1) + ": " + caixas[i]); // Exibe no console a caixa e seu conteúdo
+    // Adiciona itens à lista
+    itens.append(lista);
 }
